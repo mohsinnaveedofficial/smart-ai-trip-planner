683204da59cd23bfa1ea84cc87b4a75426522d66
mohsinnaveedofficial <mohsinnaveed196@gmail.com>
2025-09-16 22:22:19 +0500

    fix session and cookie

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ app.use(
     saveUninitialized: false,
     cookie: { 
       httpOnly: true,
-      secure:true,
-      sameSite:"none"
+      secure: process.env.NODE_ENV === "production",   
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
      },
   })
 );
